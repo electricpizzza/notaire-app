d76b0431ed0b09495250b85e49e9bf672f236c92
electricpizzza <zakariae.dinar@gmail.com>
2020-12-16 11:49:28 +0100

    fixing issues

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['authentification'] = require('../middleware/authentification.js')
-middleware['authentification'] = middleware['authentification'].default || middleware['authentification']
-
-export default middleware
--- assets/store/devisStore.js
+++
@@ -1,9 +0,0 @@
-export default {
-    articles:[],
-    dateDevis: null,
-    termes: '',
-    client: null,
-    total: 0,
-    remisG: 0,
-    
-}
